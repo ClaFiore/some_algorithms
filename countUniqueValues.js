6e2cea18ba76e322263aabdb192a0c0cb43c5604
ClaFiore <claborghini@gmail.com>
2020-10-29 17:23:30 -0400

    added second solution for countUniqueValues

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -22,3 +22,20 @@ function countUniqueValues(array){
 }
 
 
+// shorter solution:
+
+function countUniqueValues(array){
+    if (array.length === 0)
+        return 0
+    let i = 0
+    for (let j = 1; j < array.length; j++){
+        if (array[i] !== array[j]){
+            i++
+            array[i] = array[j]
+        }
+    }
+    return i + 1
+}
+
+// This has a performance of O(n) because we are only looping through the array once.
+
