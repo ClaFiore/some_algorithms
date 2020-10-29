c889ce98973c3f1a8055146208b011aac6b28b3b
ClaFiore <claborghini@gmail.com>
2020-10-29 14:45:44 -0400

    first commit

---
+++ frequency_counter.js
@@ -0,0 +1,11 @@
+// write a function 'same' which takes in two arrays and returns true 
+// if every value of the first array has a corresponding value squared in the second array, 
+// with the same frequency.
+
+function same(a,b){
+    if (a.length !== b.length) // edge case
+        return false
+    let objA = {}
+    let objB = {}
+    
+}
