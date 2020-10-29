2eee2ce0aa8a151ed1904f9f6f957e4573ab1f40
ClaFiore <claborghini@gmail.com>
2020-10-29 16:45:41 -0400

    added multiple pointers

---
+++ multiple_pointers.js
@@ -0,0 +1,7 @@
+// write a function called sumZero which accepts a SORTED array of ints
+// it returns an array of the first pair of inst where the sum is 0
+// else it returns undefined (nothing)
+
+function sumZero(arr){
+    
+}
