21e30da6a4977a736c405d712823efc4dba79d85
ClaFiore <claborghini@gmail.com>
2020-10-29 16:49:42 -0400

    function sumZero solution

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -3,5 +3,14 @@
 // else it returns undefined (nothing)
 
 function sumZero(arr){
-    
+    let i = 0
+    let j = arr.length - 1
+    while (i < j){                  // it has to be < instead of <= to exclude the possibility of 0 + 0 = 0 it would return an array of 1 int
+        if (arr[i] + arr[j] === 0)
+        return [arr[i], arr[j]]
+        else if (arr[i] < arr[j])
+            j--
+        else
+            i++
+    }
 }
