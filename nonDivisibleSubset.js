ee24cd65216e0e0e460aef4bd47fff16e6ed17f4
ClaFiore <claborghini@gmail.com>
2020-11-01 20:55:17 -0500

    added description of nondivisiblesubset

--- nonDivisibleSubset.js
+++ nonDivisibleSubset.js
@@ -4,7 +4,10 @@
 //  * The function accepts following parameters:
 //  *  1. INTEGER k
 //  *  2. INTEGER_ARRAY s
-
+//  subset can be not in order
+s = [19,10,12,10, 24, 25,22]
+k = 4
+subsets possible: [10,12,25] or []
 
 function nonDivisibleSubset(k, s) {
     let i = 0
