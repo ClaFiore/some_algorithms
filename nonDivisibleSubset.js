bf5f658ca585ead463ed9c017a3fa88459ea6aff
ClaFiore <claborghini@gmail.com>
2020-11-02 09:42:51 -0500

    added solution for nondivisiblesubset

--- nonDivisibleSubset.js
+++ nonDivisibleSubset.js
@@ -5,9 +5,12 @@
 //  *  1. INTEGER k
 //  *  2. INTEGER_ARRAY s
 //  subset can be not in order
-s = [19,10,12,10, 24, 25,22]
-k = 4
-subsets possible: [10,12,25] or []
+// EXAMPLE: 
+// s = [19,10,12,10, 24, 25,22]
+// k = 4
+// subsets possible: [10,12,25] or [19,22,24]
+// in both case max length of subarray is 3
+// function should return 3
 
 function nonDivisibleSubset(k, s) {
     let i = 0
